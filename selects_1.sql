ebf483a3a4aed9b4ceb24edba72d3848c56a1204
furetur <furetur@gmail.com>
2021-03-17 23:09:47 +0500

    added 15 sql statements

--- selects_1.sql
+++ selects_1.sql
@@ -14,6 +14,12 @@
 --     UNIQUE (x, y)
 -- )
 
+-- VIEW
+CREATE VIEW pieces_full_info AS
+SELECT *
+FROM piece_types
+         INNER JOIN pieces ON piece_types.id = pieces.type_id;
+
 -- 1
 SELECT count(type_id)
 FROM pieces;
@@ -69,16 +75,62 @@ ORDER BY t.count DESC
 LIMIT 1;
 -- 11
 -- For rook (type_id=0, type=rook, color=white, x=0, y=0)
-SELECT type, color
-from (SELECT *
-      FROM piece_types
-               INNER JOIN pieces ON piece_types.id = pieces.type_id
-      ) as t
-WHERE x = 0 OR y = 0;
+
+-- using view `pieces_full_info`
+SELECT piece.*
+FROM pieces_full_info as piece,
+     pieces_full_info as rook
+WHERE rook.type_id = 0
+  AND (piece.x = rook.x
+    OR piece.y = rook.y);
 -- 12
 SELECT color
 from (SELECT color, type, count(*) as count
       FROM piece_types
                INNER JOIN pieces ON piece_types.id = pieces.type_id
       GROUP BY color, type) AS t
-WHERE t.type = 'pawn' AND t.count = 8;
+WHERE t.type = 'pawn'
+  AND t.count = 8;
+-- 13
+SELECT *
+INTO new_pieces
+FROM pieces;
+
+UPDATE new_pieces
+SET x=5,
+    y=5
+WHERE type_id = 0; -- moves a white rook somewhere
+DELETE
+FROM new_pieces
+WHERE type_id = 8; -- deletes a white pawn
+
+SELECT *
+FROM pieces
+         LEFT JOIN new_pieces np on pieces.type_id = np.type_id
+WHERE np.type_id is null
+   OR pieces.x != np.x
+   OR pieces.y != np.y;
+-- 14
+-- using view `pieces_full_info`
+SELECT piece.*
+FROM pieces_full_info piece,
+     pieces_full_info king
+WHERE king.type_id = 28
+  AND piece.type_id != 28
+  AND abs(piece.x - king.x) <= 2
+  AND abs(piece.y - king.y) <= 2;
+-- 15
+CREATE VIEW pieces_with_distances AS
+SELECT piece.*, abs(piece.x - king.x) + abs(piece.y - king.y) as l1
+FROM pieces_full_info piece,
+     pieces_full_info king
+WHERE king.type_id = 4
+  AND piece.type_id != 4
+ORDER BY l1;
+
+CREATE VIEW smallest_l1 AS
+SELECT l1 FROM pieces_with_distances LIMIT 1;
+
+SELECT pieces_with_distances.*
+FROM pieces_with_distances, smallest_l1
+WHERE pieces_with_distances.l1 = smallest_l1.l1;
